2128d9b6e865d65fe5d22b8c5abc7886a6d7f813
Mahima Upadhyay <mahimaupadhyay.code@gmail.com>
2025-02-14 19:56:24 +0530

    commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+
 
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+ 
   return (
-    <div>
-      
+    <div className="flex justify-center my-10 ">
+      <div className="card bg-base-300 w-96 shadow-xl">
+    <div className="card-body">
+    <h2 className="card-title">Login</h2>
+   <div>  
+   <label className="form-control w-full max-w-xs my-2">
+  <div className="label">
+    <span className="label-text">Email-ID?</span>
+    
+  </div>
+  <input type="text" 
+  value={emailId} placeholder="Type here" 
+  className="input input-bordered w-full max-w-xs"
+  onChange={(e)=> setEmailId(e.target.value)} />
+  
+  
+</label>
+<label className="form-control w-full max-w-xs my-2">
+  <div className="label">
+    <span className="label-text">Password</span>
+
+    
+  </div>
+  <input type="text" value={password} placeholder="Type here" className="input input-bordered w-full max-w-xs" 
+  onChange={(e)=> setPassword(e.target.value)} />
+</label>
+   </div>
+
+    <div className="card-actions justify-center m-2">
+      <button className="btn btn-primary ">Login</button>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
 
-export default Login
+export default Login;
